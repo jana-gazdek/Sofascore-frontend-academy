49591182ef15db50254b2184dafc33c123e6e0c7
Jana Gazdek <jana.gazdek@fer.unizg.hr>
2025-03-11 23:56:43 +0100

    Nije mi dalo da commitam u homework02-branch
     Nekako sam izbrisala cijelu zadaću pa sam morala sve ispočetka i onda mi nije dalo commitati promjene u homework02 granu nego sam morala odmah u main

--- homework02/02/src/solutions.js
+++ homework02/02/src/solutions.js
@@ -2,88 +2,169 @@ module.exports = {
   /**
    * Returns an empty object without prototype. There is object creation type that creates object without prototype
    */
-  createPrototypelessObject() {},
+  createPrototypelessObject() {
+    const obj = Object.create(null);
+    return obj;
+  },
 
   /**
    * Returns an object with prototype set to given `proto`.
    * @param {Object} proto Prototype object
    */
-  createObjectWithPrototype(proto) {},
+  createObjectWithPrototype(proto) {
+    const obj = Object.create(proto);
+    return obj;
+  },
 
   /**
    * Returns an object with `value` property set to the given `value` and `getValue` method.
    * Be careful, if `value` changes, `getValue` should return changed `value`.
    * @param {any} value
    */
-  createObjectWithMethod(value) {},
+  createObjectWithMethod(value) {
+    const obj = {
+      value : value,
+      getValue : function() {
+        return this.value;
+      }
+    }
+    return obj;
+  },
 
   /**
    * Returns an object with the `getValue` and `setValue` methods, having `value` hidden from the outside.
    */
-  createEncapsulatedObject() {},
+  createEncapsulatedObject() {
+    let value; 
+    return {
+      getValue: function() {
+        return value; 
+      },
+      setValue: function(newValue) {
+        value = newValue; 
+      }
+    };
+  },
 
   /**
    * Returns the shallow copy of the given `obj`. HINT: This **operator** will be used later.
    * @param {Object} obj
    */
-  shallowCopy(obj) {},
+  shallowCopy(obj) {
+    const newObj = {... obj};
+    return newObj;
+  },
 
   /**
    * Returns the deep copy of the given `obj`.
    * @param {Object} obj
    */
-  deepCopy(obj) {},
+  deepCopy(obj) {
+    const newObj = JSON.parse(JSON.stringify(obj));
+    return newObj;
+  },
 
   /**
    * Returns an array containing 2 elements which are
    * loosely equal, but strictly unequal.
    */
-  looselyTrue() {},
+  looselyTrue() {
+    let array = [1, "1"];
+    return array;
+  },
 
   /**
    * Returns a string that is loosely equal to boolean `true`. This one is tricky :)
    */
-  stringLooselyEqualToTrue() {},
-
+  stringLooselyEqualToTrue() {
+    return '1';
+  },
   /**
    * Returns correct sum of a and b.
    */
-  safeSum(a, b) {},
+  safeSum(a, b) {
+    if(typeof a === 'string' || typeof b === 'string') {
+      a = Number(a);
+      b = Number(b);
+    }
+
+    return a + b;
+  },
 
   /**
    * Returns formatted string for the given date.
    * Format should be `{day}-{month}-{fullYear}` (all numbers).
    * @param {Date} date
    */
-  formatDate(date) {},
+  formatDate(date) {
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    return day + '-' + month + '-' + year;
+  },
 
   /**
    * Sorts the given `numberArray` in ascending order.
    * Use array `.sort` method. Sort is done in place so there is no need to return anything.
    * @param {number[]} numberArray
    */
-  sortNumberArray(numberArray) {},
+  sortNumberArray(numberArray) {
+    numberArray.sort((a, b) => a - b);
+  },
 
   /**
    * Multiplies all the elements in the array by 2 _in place_
    * (edits the given array) and returns it.
    * @param {number[]} numberArray
    */
-  multiplyArrayByTwo(numberArray) {},
+  multiplyArrayByTwo(numberArray) {
+    for(let i = 0; i < numberArray.length; i++) {
+      numberArray[i] = numberArray[i] * 2;
+    }
+
+    return numberArray;
+  },
 
   /**
    * Multiplies all the elements in the array by 2 and returns them
    * in a new array.
    * @param numberArray
    */
-  multiplyArrayByTwoNew(numberArray) {},
+
+  multiplyArrayByTwoNew(numberArray) {
+    let newArray = [];
+    for(let i = 0; i < numberArray.length; i++) {
+      newArray[i] = numberArray[i] * 2;
+    }
+
+    return newArray;
+  },
 
   /**
    * Returns first `n` Fibonacci numbers in an array. https://en.wikipedia.org/wiki/Fibonacci_sequence
    * If the n is <= 0, return `undefined`
    * @param n
    */
-  fibonacciNumbers(n) {},
+
+  fibonacciNumbers(n) {
+    let fib = [];
+    if(n <= 0) {
+      return undefined;
+    }
+
+    if(n >= 1) {
+      fib[0] = 0;
+    }
+
+    if(n > 1) {
+      fib[1] = 1;
+      for(let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+      }
+    }
+    return fib;
+  },
 
   /**
    *
@@ -99,7 +180,33 @@ module.exports = {
    * @param {Function} callGetName
    * @param {Function} callGetLanguage
    */
-  classInheritance(callGetName, callGetLanguage) {},
+
+  classInheritance(callGetName, callGetLanguage) {
+    class Person {
+      constructor(name) {
+          this.name = name;
+          this.getName = this.getName.bind(this);
+      }
+
+      getName() {
+          return callGetName(this.name);
+      }
+  }
+
+  class Programmer extends Person {
+      constructor(name, language) {
+          super(name);
+          this.language = language;
+          this.getLanguage = this.getLanguage.bind(this);
+      }
+
+      getLanguage() {
+          return callGetLanguage(this.language);
+      }
+  }
+
+  return { Person, Programmer };
+  },
 
   /**
    * **This is variant of probably most common "big firm" interview question with closures.**
@@ -113,11 +220,13 @@ module.exports = {
    * Use the provided for loop, do not change for loop, but feel free to modify setTimeout.
    * @param {Function} consumer
    */
+
+  // Ovaj zadatak nisam znala :(
+  // Ali sam nabacila copy-paste s Google-a jer me smetalo sto mi baca jedan krivi test :D
+  // Obecajem da sam shvatila zasto je to tako!!
   timeoutIncrement(consumer) {
     for (var i = 1; i <= 3; i += 1) {
-      setTimeout(() => {
-        /* your function goes here, or instead of this function */
-      }, 1000)
+      setTimeout(((i) => () => consumer(i))(i), i * 1000);
     }
   },
 }
